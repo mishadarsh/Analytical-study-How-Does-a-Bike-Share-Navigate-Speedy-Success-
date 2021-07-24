debb47650e1251332d4e071591f7c100b5ca409c
mishadarsh <85312357+mishadarsh@users.noreply.github.com>
2021-07-24 15:36:08 +0530

    Add files via upload

---
+++ cyclistic_ride.R
@@ -0,0 +1,71 @@
+install.packages("tidyverse")
+library("tidyverse")
+install.packages("ggplot2")
+library("ggplot2")
+install.packages("lubridate")
+library("lubridate")
+install.packages("dtplyr")
+library("dtplyr")
+install.packages("dplyr")
+library("dplyr")
+install.packages("readr")
+library("readr")
+install.packages("plyr")
+library("plyr")
+install.packages("rlang")
+library("rlang")
+getwd()
+setwd("/Users/Adarsh Mishra/OneDrive/Desktop/cyclistic_R/CSV FILES")
+q3_2019 <- read_csv("Divvy_Trips_2019_Q3.csv")
+q4_2019 <- read_csv("Divvy_Trips_2019_Q4.csv")
+q1_2020 <- read_csv("Divvy_Trips_2020_Q1.csv")
+colnames(q3_2019)
+colnames(q4_2019)
+colnames(q1_2020)
+
+
+(q4_2019 <- rename(q4_2019
+                   ,ride_id = trip_id
+                   ,rideable_type = bikeid 
+                   ,started_at = start_time  
+                   ,ended_at = end_time  
+                   ,start_station_name = from_station_name 
+                   ,start_station_id = from_station_id 
+                   ,end_station_name = to_station_name 
+                   ,end_station_id = to_station_id 
+                   ,member_casual = usertype))
+
+(q3_2019 <- rename(q3_2019
+                   ,ride_id = trip_id
+                   ,rideable_type = bikeid 
+                   ,started_at = start_time  
+                   ,ended_at = end_time  
+                   ,start_station_name = from_station_name 
+                   ,start_station_id = from_station_id 
+                   ,end_station_name = to_station_name 
+                   ,end_station_id = to_station_id 
+                   ,member_casual = usertype))
+str(q1_2020)
+str(q4_2019)
+str(q3_2019)
+
+q4_2019 <-  mutate(q4_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
+q3_2019 <-  mutate(q3_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
+all_trips <- bind_rows(q3_2019, q4_2019, q1_2020)
+all_trips <- all_trips %>%  
+  select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender, "01 - Rental Details Duration In Seconds Uncapped", "05 - Member Details Member Birthday Year", "Member Gender", "tripduration"))
+
+colnames(all_trips)  #List of column names
+nrow(all_trips)  #How many rows are in data frame?
+dim(all_trips)  #Dimensions of the data frame?
+head(all_trips)  #See the first 6 rows of data frame.  Also tail(qs_raw)
+str(all_trips)  #See list of columns and data types (numeric, character, etc)
+summary(all_trips)  #Statistical summary of data. Mainly for numerics
+
+
+
+
+
+
